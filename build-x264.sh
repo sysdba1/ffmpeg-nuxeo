989d6254475056ca8978cf246295a6a027f5528b
sysdba1 <sysdba1@users.noreply.github.com>
2018-06-06 21:33:37 +0400

    Update build-x264.sh

--- build-x264.sh
+++ build-x264.sh
@@ -17,7 +17,7 @@ fi
 
 pushd x264
 git checkout $BRANCH
-./configure --enable-static
+./configure --enable-shared
 make
 VERSION=$(./version.sh | awk -F'[" ]' '/POINT/{print $4"+git"$5}')
 echo "x264 - Nuxeo version" > description-pak
